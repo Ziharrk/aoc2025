62b5661ffca6bedea8ce0cad681ad3806f303318
Kai-Oliver Prott <github@kaiprott.de>
2025-12-05 12:18:34 +0100

    Add day5

--- src/Day5.hs
+++ src/Day5.hs
@@ -2,7 +2,29 @@ module Day5 where
 
 day5 :: IO ()
 day5 = do
+  (inputR, inputIDs) <- break null . lines <$> readFile "input/day5"
+  let ranges = foldr (addRange . toRange) [] inputR
+      ids = map read $ drop 1 inputIDs
   putStr "Part 1: "
-  print ()
+  let fresh = filter (inRanges ranges) ids
+  print (length fresh)
   putStr "Part 2: "
-  print ()
+  print (sum $ map rangeSize ranges)
+
+toRange :: String -> (Integer, Integer)
+toRange s = (read l, read $ drop 1 h)
+  where
+    (l, h) = break (=='-') s
+
+addRange :: (Integer, Integer) -> [(Integer, Integer)] -> [(Integer, Integer)]
+addRange (l, h) [] = [(l, h)]
+addRange (l, h) rs@((l', h'):rs')
+  | h < l'    = (l , h ) : rs
+  | l > h'    = (l', h') : addRange (l, h) rs'
+  | otherwise = addRange (min l l', max h h') rs'
+
+inRanges :: [(Integer, Integer)] -> Integer -> Bool
+inRanges rs i = any (\(l, h) -> l <= i && i <= h) rs
+
+rangeSize :: (Integer, Integer) -> Integer
+rangeSize (l, h) = h-l+1
