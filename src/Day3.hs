388a91b6c58b5786036bf2fe9bafad385a39e718
Kai-Oliver Prott <github@kaiprott.de>
2025-12-08 16:27:17 +0100

    Simplify day3

--- src/Day3.hs
+++ src/Day3.hs
@@ -2,22 +2,19 @@ module Day3 where
 
 day3 :: IO ()
 day3 = do
-  input <- map (map (read . return)) . lines <$> readFile "input/day3"
+  input <- lines <$> readFile "input/day3"
   putStr "Part 1: "
-  print (sum (map (fromList . maxJoltsWith  2) input))
+  print (sum (map (read @Int . maxJoltsWith  2) input))
   putStr "Part 2: "
-  print (sum (map (fromList . maxJoltsWith 12) input))
+  print (sum (map (read @Int . maxJoltsWith 12) input))
 
-maxJoltsWith :: Int -> [Integer] -> [Integer]
+maxJoltsWith :: Int -> [Char] -> [Char]
 maxJoltsWith 0 _  = []
 maxJoltsWith k xs = m : maxJoltsWith k' xs'
   where
     k' = k-1
     m = maximum (dropBack k' xs)
     xs' = drop 1 $ dropWhile (/= m) xs
-
-fromList :: [Integer] -> Integer
-fromList = foldl (\s d -> s * 10 + d) 0
 
 dropBack :: Int -> [a] -> [a]
 dropBack n = reverse . drop n . reverse
