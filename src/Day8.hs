c7441343c6e51f7f32480626eb5e8d2543787eb0
Kai-Oliver Prott <github@kaiprott.de>
2025-12-09 02:28:41 +0100

    Simplify day8

--- src/Day8.hs
+++ src/Day8.hs
@@ -7,39 +7,34 @@ import Data.List (sortBy, sortOn)
 import Data.List.Extra (splitOn)
 import Data.Ord (comparing, Down (..))
 import qualified Data.Set as Set
-import Data.Tuple.Extra (thd3)
 
-import Utils (allPairs, iterateNM, untilAll)
+import Utils (allPairs)
 
 type C3 = (Int, Int, Int)
 
 day8 :: IO ()
 day8 = do
   input <- map (triple . map read . splitOn ",") . lines <$> readFile "input/day8"
-  let withDists = sortOn thd3 [ (e1, e2, d) | (e1, e2) <- allPairs input
-                                            , let d = distSq3 e1 e2]
+  let sorted = sortOn (uncurry distSq3) (allPairs input)
   putStr "Part 1: "
   let eqs = runEquivM Set.singleton Set.union $
-              iterateNM 1000 (addToEquivalence return) withDists
+              mapM_ (addToEquivalence return) (take 1000 sorted)
               >> classes >>= mapM desc -- extract equivalence classes
       sortedEqs = take 3 $ sortBy (comparing Down) (map Set.size eqs)
   print (product sortedEqs)
   putStr "Part 2: "
   let ((x1,_,_), (x2,_,_)) = flip execState (error "everything already equivalent") $
-        runEquivT' $ untilAll (addToEquivalence put) withDists
+        runEquivT' $ mapM_ (addToEquivalence put) (take 10000 sorted)
   print (x1 * x2)
 
-{-# INLINE addToEquivalence #-}  -- ^ saves ~0.5s due to specialization on type of m
 addToEquivalence :: MonadEquiv c C3 d m
                  => ((C3, C3) -> m a) -- ^ action to execute for already equivalent values
-                 -> [(C3, C3, Int)]   -- ^ sorted list of point pairs and their distances
-                 -> m [(C3, C3, Int)]
-addToEquivalence _ [] = return []
-addToEquivalence act ((x,y,_):xs) = do
-  b <- equivalent x y
-  unless b $ void $ act (x, y)
-  equate x y
-  return xs
+                 -> (C3, C3)
+                 -> m ()
+addToEquivalence act (c1, c2) = do
+  b <- equivalent c1 c2
+  unless b $ void $ act (c1, c2)
+  equate c1 c2
 
 triple :: [Int] -> C3
 triple [x, y, z] = (x, y, z)
